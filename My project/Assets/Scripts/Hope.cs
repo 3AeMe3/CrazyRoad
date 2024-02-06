b70daacbb54ba47268097b8e9d3b6487f81c1d09
3AeMe3 <fm792086@gmail.com>
2024-02-05 21:31:34 -0500

    a

---
+++ My project/Assets/Scripts/Hope.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hope : MonoBehaviour
+{
+    [Range(0, 10)]
+    [SerializeField] float speed;
+
+   [Range(0, 5)]
+    [SerializeField] float distance;
+
+
+    public void StartMove(Vector3 direction)
+    {
+
+    }
+  
+}
---
+++ My project/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        { 
+
+        }else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+        }else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+        }
+    }
+}
