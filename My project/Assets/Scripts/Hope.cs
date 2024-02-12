6a7efe59bbb47e464b5141a400cf31852f698d26
3AeMe3 <fm792086@gmail.com>
2024-02-12 13:08:27 -0500

    12/02 - 1:07

--- My project/Assets/Scripts/Hope.cs
+++ My project/Assets/Scripts/Hope.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
 public class Hope : MonoBehaviour
@@ -7,13 +8,48 @@ public class Hope : MonoBehaviour
     [Range(0, 10)]
     [SerializeField] float speed;
 
-   [Range(0, 5)]
+    [Range(0, 5)]
     [SerializeField] float distance;
+    [Range(0,5)]
+    [SerializeField] float height;
+
+    [SerializeField] bool rotation;
+
+    public bool Ishopping { get; private set; }
 
 
     public void StartMove(Vector3 direction)
     {
-
+        Ishopping = true;
+        if (rotation)
+        {
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
+        Vector3 targetPos = transform.position + direction * distance;
+        StartCoroutine(Move(targetPos));
     }
-  
+    IEnumerator Move(Vector3 targetPos)
+    {
+        Vector3 startPos = transform.position;
+        float movePorcentage = 0;
+        while (movePorcentage <= 1.0f)
+        {
+            movePorcentage += speed * Time.deltaTime;
+            transform.position = Vector3.Lerp(startPos, targetPos, movePorcentage);
+
+            //jumping
+            float jumpPercentage = CalculateJumpPercentage(movePorcentage);
+            transform.position = new Vector3(transform.position.x, startPos.y + jumpPercentage * height, transform.position.z);
+
+            yield return null;
+        }
+        transform.position = targetPos;
+        Ishopping = false;
+    }
+    float CalculateJumpPercentage(float movePercentage)
+    {
+        float jumpPercentage = movePercentage * 2;
+        return Mathf.Lerp(0,1, -jumpPercentage * jumpPercentage + 2 * jumpPercentage);
+    }
+
 }
--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -1,25 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
-
+    Hope hope;
+    private void Awake()
+    {
+        hope = GetComponent<Hope>();
+    }
     private void Update()
     {
+        if (hope.Ishopping)
+        {
+            return; 
+        }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-
+            hope.StartMove(Vector3.left);
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
-        { 
-
-        }else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
+            hope.StartMove(Vector3.right);
 
-        }else if (Input.GetKeyDown(KeyCode.DownArrow))
+
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
+            hope.StartMove(Vector3.forward);
+
+
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            hope.StartMove(Vector3.back);
 
         }
     }
